6bfa512839990fc95e61e0145d431fdd7f2eea11
Akash <akashappa.te@gmail.com>
2024-09-08 16:17:52 -0500

    Added CQRS (Building Blocks)

---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace CatalogAPI.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string Image, decimal Price) : IRequest<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
